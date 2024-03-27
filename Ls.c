e156602a8a60e7d1aba822b28dcbc78381ec4fce
Sara_Xavier <saraaxavierrr@gmail.com>
2024-03-27 09:48:11 -0300

    Update

---
+++ Ls.c
@@ -0,0 +1,3 @@
+#include <stdio.h>      // Biblioteca de entrada/saída padrão de C
+#include <stdlib.h>     // Biblioteca de informações sobre o sistema, memória dinâmica e processos
+#include <dirent.h>     // Biblioteca para manipulação de diretórios em C.
