35f06dda93ada25fa46c8a126ca99fe1925fa778
Sara_Xavier <saraaxavierrr@gmail.com>
2024-03-27 10:15:46 -0300

    Update de programa. Eu havia esquecido que precisava usar o getcwd

--- Ls.c
+++ Ls.c
@@ -1,18 +1,34 @@
 #include <stdio.h>      // Biblioteca de entrada/saída padrão de C
 #include <stdlib.h>     // Biblioteca de informações sobre o sistema, memória dinâmica e processos
 #include <dirent.h>     // Biblioteca para manipulação de diretórios em C.
+#include <unistd.h>     // Biblioteca da função 'getcwd'
 
 
 int main(int argc, char *argv[]){
-    if (argc != 2){
-        fprintf(stderr, "Uso correto> %s <diretório>/n", argv[0]); //Verificando se argumento está correto
+    char *dir_path;
+
+    if (argc == 1){
+        dir_path = getcwd(NULL, 0); //Verificando se diretorio está como argumento, se nenhumn for verificado abre o diretorio atual
+        if (dir_path == NULL){
+            perror("Erro ao abrir diretório de trabalho atual");
+            return 1;
+        }
+        
+    }
+
+    else if(argc == 2){
+        dir_path == argv[1];
+    }
+
+    else{ //Else criado para não receber mais de um argumento. Caso isso aconteça, dará erro
+        fprintf(stderr, "Uso correto: %s [diretorio]/n", argv[0]);
         return 1;
     }
 
-    DIR *dir = opendir(argv[1]); // Especificação de argumento para abrir diretório pela linha de comando
+    DIR *dir = opendir(dir_path):
     if (dir == NULL){
         perror("Erro ao abrir diretório");
-        return 1;
+        free(dir_path);  //Liberando memória alocada por 'getcwd()'
     }
 
     struct dirent *entry;
@@ -21,6 +37,7 @@ int main(int argc, char *argv[]){
     }
 
     closedir(dir);               // Fecha diretório
+    free(dir_path);              // Libera memória alocada
     return 0;
 
 }
