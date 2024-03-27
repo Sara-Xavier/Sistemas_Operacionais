67c16c0240d9e3c768d32612e95d56be6a9ac7f5
root <root@cnat407138A.ifrn.local>
2024-03-27 08:01:29 -0300

    Correção de erros do programa

--- Programas/Uptime.c
+++ Programas/Uptime.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
-#include <sys/sysinfo.h> #Biblioteca de informações sobre o sistema
+#include <stdlib.h> #Biblioteca de informações sobre o sistema
 #include <time.h>
 
-int main(){
-    FILE *uptimeFile* = fopen("/proc/uptime", "r");
+int main() {
+    FILE *uptimeFile = fopen("/proc/uptime", "r");
     if (uptimeFile == NULL) {
-        fprint(stderr, "Erro ao abrir arquivo /proc/uptime");
+        fprintf(stderr, "Erro ao abrir o arquivo /proc/uptime\n");
         return 1;
     }
-    
+
     double uptimeSeconds;
     if (fscanf(uptimeFile, "%lf", &uptimeSeconds) != 1) {
         fprintf(stderr, "Erro ao ler o tempo de atividade\n");
         fclose(uptimeFile);
         return 1;
     }
+
     fclose(uptimeFile);
 
-    int uptimeHours = (int) (uptimeSeconds / 3600);
-    int uptimeMinutes = (int) ((uptimeSeconds * 3600) / 60);
-    int uptimeSecondsRemainder = (int) (uptimeSeconds - uptimeHours * 3600 - uptimeMinutes * 60);
+    int uptimeHours = (int)(uptimeSeconds / 3600);
+    int uptimeMinutes = (int)((uptimeSeconds - uptimeHours * 3600) / 60);
+    int uptimeSecondsRemainder = (int)(uptimeSeconds - uptimeHours * 3600 - uptimeMinutes * 60);
 
     printf("Tempo de atividade do sistema: %dh %dm %ds\n", uptimeHours, uptimeMinutes, uptimeSecondsRemainder);
+
     return 0;
 }
