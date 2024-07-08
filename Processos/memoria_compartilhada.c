d926933023563ba66cfce0ea16b27672c52f9973
Sara Xavier Pires Lucena da Silva <20211014040055@ifrn.local>
2024-07-08 07:52:50 -0300

    Memória compartilhada

---
+++ Processos/memoria_compartilhada.c
@@ -0,0 +1,21 @@
+/* produção de dados*/
+
+item next_produced;
+while (1){
+    /*Produz item e armazena em next_produced*/
+    while(((in + 1) % BUFFER_SIZE) == out)
+    ;  /*buffer lotado. Espera ocupada fazendo nada*/
+    buffer[in] = next_produced;
+    in = (in + 1) % BUFFER_SIZE;
+}
+
+/* consumo de dados*/
+
+item next_consumed;
+while (1){
+    while (in == out)
+    ; /*Buffer vazio. Espera fazendo nada.*/
+    next_consumed = buffer[out];
+    out = (out + 1) % BUFFER_SIZE;
+    /*Consome o item armazenado em next_consumed*/
+}
